e424afb9862349bdef304a9dbc1b876ba3d9d7ad
mohandtaharb <lemamouth2@gmail.com>
2023-08-11 10:54:16 +0100

    Fix CORS

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,45 @@ pub mod routes;
 pub mod schema;
 use routes::*;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, Method, Status};
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        if request.method() == Method::Options {
+            response.set_status(Status::NoContent);
+            response.set_header(Header::new(
+                "Access-Control-Allow-Methods",
+                "POST, PATCH, GET, DELETE",
+            ));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        }
+
+        response.set_header(Header::new(
+            "Access-Control-Allow-Origin",
+            "*",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-    .register("/", catchers![not_found])
-    .mount(
-    "/api",
+
+    rocket::build().attach(CORS).register("/", catchers![not_found]).mount(
+        "/api",
         routes![
             get_pending_tasks,
             get_accomplished_tasks,
